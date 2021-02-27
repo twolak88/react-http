4240249d904ecd8774636348b30b54a39091d267
twolak88 <twolak88@gmail.com>
2021-02-27 20:06:54 +0100

    Making a Post Selectable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,7 +7,8 @@ import axios from 'axios';
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   }
 
   componentDidMount() {
@@ -27,12 +28,19 @@ class Blog extends Component {
       });
   }
 
+  postSelectedHandler = (id) => {
+    this.setState({
+      selectedPostId: id
+    });
+  }
+
   render() {
     const posts = this.state.posts.map(post => {
       return <Post 
               key={post.id} 
               title={post.title}
-              author={post.author}/>
+              author={post.author}
+              clicked={() => this.postSelectedHandler(post.id)}/>
     });
     return (
       <div>
@@ -40,7 +48,7 @@ class Blog extends Component {
           {posts}
         </section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId}/>
         </section>
         <section>
           <NewPost />
