92dc61bdcd007718c7d300a3382ff68723cb841c
twolak88 <twolak88@gmail.com>
2021-02-27 21:50:10 +0100

    Setting a Default Global Configuration for Axios

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 axios.interceptors.request.use(request => {
   console.log(request);
   // Edit request config
