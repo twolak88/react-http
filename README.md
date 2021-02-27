2bfa7dc15fa8f471bedad7c3f4f85d6a0bea9e57
twolak88 <twolak88@gmail.com>
2021-02-27 19:11:17 +0100

    Creating a Http Request to GET Data

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+#Backend dummy server for HTTP Requests
+
+https://jsonplaceholder.typicode.com/posts
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,8 +3,17 @@ import classes from './Blog.module.css';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
+import axios from 'axios';
 
 class Blog extends Component {
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        console.log(response);
+      });
+  }
+
   render() {
     return (
       <div>
